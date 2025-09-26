dafbdad1a51aa55278324e15ee777d3ddce3a1d7
Saurabh Dombe <saurabhdombe83@gmail.com>
2025-09-26 19:39:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+ const sounds = ["sound1", "sound2", "sound3", "sound4", "sound5", "sound6"];
+
+    // Create audio elements dynamically
+    const audios = {};
+    sounds.forEach(name => {
+      audios[name] = new Audio(`./sounds/${name}.mp3`);
+    });
+
+    // Select buttons
+    const buttons = document.querySelectorAll(".btn");
+    const stopBtn = document.querySelector(".stop");
+
+    // Add event listeners
+    buttons.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        stopAll();
+        audios[sounds[index]].play();
+      });
+    });
+
+    stopBtn.addEventListener("click", stopAll);
+
+    function stopAll() {
+      sounds.forEach(name => {
+        audios[name].pause();
+        audios[name].currentTime = 0; // Reset to start
+      });
+    }
